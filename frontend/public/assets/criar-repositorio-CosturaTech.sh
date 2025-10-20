ec51497f906547ab4e4ef019c6107544088461bf
Eduardo Ortiz Passos <ortizpassos@yahoo.com.br>
2025-10-20 19:46:09 -0300

    Ajuste css

--- frontend/public/assets/criar-repositorio-CosturaTech.sh
+++
@@ -1,8 +0,0 @@
-cd "C:\projetos\CosturaTech"
-git init
-git add .
-git commit -m "Criação do repositório"
-git remote add origin https://github.com/ortizpassos/CosturaTech.git
-gh repo create CosturaTech --public --source=. --push --description "Projetos com CosturaTech"
-git branch -M main
-git push -u origin main
